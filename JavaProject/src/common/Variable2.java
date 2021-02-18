14aa1e2804cf3f5999a17bb4c6b49997a57a6b1a
admin <admin@YD01-14>
2021-02-18 12:40:42 +0900

    first git

---
+++ JavaProject/src/common/Variable2.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Variable2 {
+	public static void main(String[] args) {
+		int a, b, result;
+		a = 100;
+		b = 200;
+		result = a + b;
+		System.out.println("결과값은 " + result + " 입니다. ");	
+	}
+
+}
